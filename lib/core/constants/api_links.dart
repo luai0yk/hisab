b46c5a15aff21a61025a4b0777371417431831b8
luai0yk <loaikalesi@gmail.com>
2025-05-31 17:12:53 +0300

    Ignore api_links.dart

--- lib/core/constants/api_links.dart
+++
@@ -1,5 +0,0 @@
-abstract class ApiLinks {
-  static const String _serverName = "http://10.0.2.2/WebProjects/hisab";
-  static const String signUpLink = '$_serverName/auth/create.php';
-  static const String loginLink = '$_serverName/auth/read.php';
-}
